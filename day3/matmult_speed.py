bae8176c19aab415cd1c01ac0bb4b8fe201c8001
ZgtL15 <zgtl15@foxmail.com>
2024-02-21 22:44:55 +0100

    speed matmult_speed.py

--- day3/matmult_speed.py
+++ day3/matmult_speed.py
@@ -1,17 +1,19 @@
 # Program to multiply two matrices using nested loops
-#import random
+import random
 import numpy as np # import np
 
 N = 250
 
 # NxN matrix
 X=[]
-for i in range(N):
-    X.append([random.randint(0,100) for r in range(N)])
+X = np.random.randint(0,100, size=(N,N))
 
 
+#Nx(N+1) matrix
+Y=[]
+Y = np.random.randint(0,100, size=(N,N+1))
 
-# Nx(N+1) matrix
+
 #for i in range(N):
 #    Y.append([random.randint(0,100) for r in range(N+1)])
 
@@ -19,10 +21,21 @@ for i in range(N):
 #    Y.append([np.random.randint(0,100) for r in range(N+1)])
 
 
+result = np.zeros(shape = (N,N+1),dtype = int)
+result += np.matmul(X,Y)
+[print(r) for r in result]
+
+
+
+
+
+
+
+
 
 # result is Nx(N+1)
 #result = []
-#for i in range(N):
+#for i in rang):
 #    result.append([0] * (N+1))
 
 # iterate through rows of X
